76472ec5d8a02445fcfd089d8fa1949b370f015c
Sea <627247084@qq.com>
2021-05-17 18:37:44 +0800

    fix

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 
 import { SearchPanel } from './search-panel'
 import { List } from './list'
-import { cleanObject } from '../../utils'
+import { cleanObject, useMount } from '../../utils'
 import * as qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -27,13 +27,13 @@ export const ProjectListScreen = () => {
     )
   }, [param])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
 
   return (
     <div>
